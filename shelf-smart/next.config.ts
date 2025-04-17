81d32eed4e1af3ad5d07577fd91563eba1854df8
Albert Joseph <dfweq@users.noreply.github.com>
2025-04-17 13:22:19 -0400

    implement security - api rate limiting and security for photo upload

--- shelf-smart/next.config.ts
+++ shelf-smart/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return process.env.NODE_ENV === 'production' ? [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'http://' }],
+        destination: `https://${process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, '')}/:path*`,
+        permanent: true,
+      },
+    ] : [];
+  },
 };
 
 export default nextConfig;
